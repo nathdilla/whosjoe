9fcb6038f62f025149d4d726052a08ccc4b2e260
nxthando <natedillamc@gmail.com>
2019-12-10 14:01:18 -0800

    updated Plant class and implemented planting system.

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -13,7 +13,10 @@ public class MyGdxGame extends ApplicationAdapter {
 	private Texture idle;
 	private Texture walkA;
 	private Texture walkB;
+	private Texture plantZero;
 	public Sprite mySprite;
+	public Sprite myPlant;
+	public PlantInventory plantInventory;
 	private int X;
 	private int Y;
 
@@ -23,31 +26,53 @@ public class MyGdxGame extends ApplicationAdapter {
 		idle = new Texture("joe.png");
 		walkA = new Texture("joeWalkA.png");
 		walkB = new Texture("joeWalkB.png");
+		plantZero = new Texture("littlePlant.png");
 		mySprite = new Sprite(idle, idle.getWidth(), idle.getHeight());
+		myPlant = new Sprite(plantZero, plantZero.getWidth()/2, plantZero.getHeight()/2);
+
+		plantInventory = new PlantInventory();
 	}
 
 	@Override
 	public void render () {
+		
 		if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
 		{
 			X--;
+			mySprite.setX(X);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
 		{
 			X++;
+			mySprite.setX(X);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.DOWN))
 		{
+
 			Y--;
+			mySprite.setY(Y);
 		}
 		if (Gdx.input.isKeyPressed(Input.Keys.UP))
 		{
 			Y++;
+			mySprite.setY(Y);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE))
+		{
+			float[][] Position = new float[(int) mySprite.getX()][(int) mySprite.getY()];
+			plantZero = new Texture("littlePlant.png");
+			Sprite createPlant = new Sprite(plantZero, plantZero.getWidth()/2, plantZero.getHeight()/2);
+			Plant newPlant = new Plant("Rose", 0, 100, createPlant, plantZero, Position);
+			plantInventory.addPlant(newPlant);
 		}
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(mySprite, X, Y);
+		for (int i = 0; i < plantInventory.plants.size(); i++)
+		{
+			batch.draw(plantInventory.plants.get(i).getTexture(),plantInventory.plants.get(i).Position.length, plantInventory.plants.get(i).Position[0].length);
+		}
 		batch.end();
 	}
 	
--- core/src/com/mygdx/game/Plant.java
+++ core/src/com/mygdx/game/Plant.java
@@ -1,17 +1,55 @@
 package com.mygdx.game;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import javafx.geometry.Pos;
+
+import javax.swing.text.Position;
+import java.util.ArrayList;
 
 public class Plant
 {
     private String type;
     private int age;
     private int hydration;
+    public Sprite sprite;
+    public Texture texture;
+    public ArrayList<Plant> plants = new ArrayList<Plant>();
+    public float[][] Position;
 
-    public Plant(String type, int age, int hydration)
+    public Plant(String type, int age, int hydration, Sprite sprite, Texture currentTexture, float[][] Position)
     {
         this.age = age;
         this.type = type;
         this.hydration = hydration;
+        this.sprite = sprite;
+        this.texture = currentTexture;
+        this.Position = Position;
+        //plants.add();
+    }
+    public Texture nextFace(int age)
+    {
+        return new Texture("littlePlant");
+    }
+    public Sprite getSprite(Sprite sprite)
+    {
+        return sprite;
+    }
+    public float[][] getPosition()
+    {
+        return Position;
+    }
 
-        
+    public Texture getTexture() {
+        return texture;
+    }
+    public void updatePosition(int x,int y, int incrementX, int incrementY)
+    {
+        float[][] Position = new float[x + incrementX][y+ incrementY];
+        this.Position = Position;
     }
 }
